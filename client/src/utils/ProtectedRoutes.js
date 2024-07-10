dff7b23696571494edc9da0ec0e32cd819e4f42d
khanhanh3bg <khanhbi345@gmail.com>
2024-07-10 15:35:11 +0700

    fix login super

--- client/src/utils/ProtectedRoutes.js
+++ client/src/utils/ProtectedRoutes.js
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoutes = () => {
-  const isAuthenticated = localStorage.getItem('userRole') === 'admin';
+  const isAuthenticated = localStorage.getItem('userRole') === 'admin' || localStorage.getItem('userRole') === 'super';
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
